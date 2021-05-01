47bbf935accff3a4c2b3e07f49ad15cdd0113323
JABUCKLE26 <james.buckley@nology.io>
2021-05-01 18:19:06 +0100

    Bot message delete commands.

--- src/Bot.py
+++ src/Bot.py
@@ -1,12 +1,12 @@
 # bot.py
 import os
-
 import discord
+from discord.ext import tasks
 from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
 
 client = discord.Client()
 
@@ -15,5 +15,21 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
+@client.event
+async def on_message(message_event):
+    if message_event.content == '/snap':
+        await schedule_delete(message_event.channel)
+    if message_event.content == '/starttimer':
+        schedule_delete.start(message_event.channel)
+    if message_event.content == '/endtimer':
+        schedule_delete.stop(message_event.channel)
+
+
+@tasks.loop(seconds=30)
+async def schedule_delete(channel):
+    messages = await channel.history(limit=300).flatten()
+    for message in messages:
+        await message.delete()
 
 client.run(TOKEN)
